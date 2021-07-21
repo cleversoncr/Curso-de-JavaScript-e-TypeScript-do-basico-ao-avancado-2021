9b8a2c5be699e3bf48c574ceafcfe3e2c56ed3a8
cleversoncr <cleversoncr@gmail.com>
2021-07-20 23:47:20 -0300

    Exercícios parte 2 20/7

---
+++ aula17/js/index.js
@@ -0,0 +1,39 @@
+const raiz = n => n ** 0.5;
+
+// const raiz = (n) => {
+//     return n ** 0.5;
+// };
+
+// const raiz = function(n) {
+//     return n ** 0.5;
+// };
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+
+
+
+
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// console.log(soma(2, 2));
+// console.log(soma(3, 1));
+// console.log(soma(5, 10));
+
+
+// function saudacao(nome) {
+//     return `Bom dia ${nome}!`;
+// }
+
+// const variavel = saudacao('Cleverson')
+// console.log(variavel);
+
+
+// saudacao('Cleverson');
+// saudacao('Jhosef');
+// saudacao('Jhonny');
---
+++ aula19/js/index.js
@@ -0,0 +1,7 @@
+/*
+Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+valores copiados
+
+Referência (mutáveis) - array, object, funciton
+passados por referência
+*/
