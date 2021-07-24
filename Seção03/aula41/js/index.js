090831d3a3181e952c2cf925aea09394b1424b6d
cleversoncr <cleversoncr@gmail.com>
2021-07-23 23:08:21 -0300

    modelo para as próximas aulas

---
+++ Seção03/aula41/js/index.js
@@ -0,0 +1,17 @@
+const numero = 12;
+
+// if (numero >= 0 && numero <= 5) {
+//   console.log('O número está entre 0 e 5.');
+// } else {
+//   console.log('O númnero NÂO está entre 0 e 5.');
+// }
+
+if (numero >= 0 && numero <= 5) {
+  console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+  console.log('O númer está entre 6 e 8.');
+} else if (numero >= 9 && numero <= 11) {
+  console.log('O númnero está entre 9 e 11.');
+} else {
+  console.log('O númnero NÂO está entre 0 e 11.');
+}
