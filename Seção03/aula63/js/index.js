59e5c17fb1c495aebfab85be56a0dd843dd48824
cleversoncr <cleversoncr@gmail.com>
2021-08-03 09:19:59 -0300

    Aulas 03/08

---
+++ Seção03/aula63/js/index.js
@@ -0,0 +1,19 @@
+// try {
+//   console.log(naoExisto);
+// } catch (error) {
+//   console.log('naoExisto nâo existe.');
+// }
+
+function soma(x, y) {
+  if (typeof x !== 'number' || typeof y !== 'number') {
+    throw ('x e y precisam ser números.');
+  }
+  return x + y;
+}
+
+try {
+  console.log(soma(1, 2));
+  console.log(soma('1', 2));
+} catch (error) {
+  console.log(error);
+}
